57c284347af8701b37175ba0cd430a08fc767cc8
Soledad Pizarro <soledad.pizarro21@gmail.com>
2024-03-13 01:25:34 -0300

    img agregadas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import cartsRouter from "./routes/carts.router.js"
 import productsRouter from "./routes/products.router.js"
+import ProductManager from "./productManager.js";
 
 const app = express();
+const productManager = new ProductManager("../data/products.json");
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -15,9 +17,10 @@ app.get('/', (req, res) => {
 //ROUTES
 app.use("/api/products", productsRouter)
 app.use("/api/carts", cartsRouter)
+app.use(express.static("public"));
 
 //PORT LISTEN
 const port = 8800;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Servidor activo http://localhost: ${port}`)
 })
